678f9032171d29ceb53122376673bc501474331b
Chel <32721235+Sililbsihi@users.noreply.github.com>
2026-05-14 20:26:00 +0800

    Update input-group.tsx

--- src/components/ui/input-group.tsx
+++ src/components/ui/input-group.tsx
@@ -2,6 +2,8 @@
 
 import { Input } from '@/components/ui/input';
 
-export function InputGroup(props: React.ComponentProps<'typeof Input'>) {
+export function InputGroup(props: React.ComponentProps<'input'>) {
   return <Input {...props} />;
 }
+
+export default InputGroup;
