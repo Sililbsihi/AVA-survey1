0af171ac8403295fe28859d2aa7c524eff22172e
Chel <32721235+Sililbsihi@users.noreply.github.com>
2026-05-15 14:46:11 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,16 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ['*.dev.coze.site'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
